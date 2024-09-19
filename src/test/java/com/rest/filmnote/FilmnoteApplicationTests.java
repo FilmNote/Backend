4e6145175ae9cb1a08df73ebf222785a45fdb98c
Jeongmin <srm0609@naver.com>
2024-09-19 18:00:59 +0900

    initail commit

---
+++ src/test/java/com/rest/filmnote/FilmnoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.filmnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmnoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
